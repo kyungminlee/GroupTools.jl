a138b1433d8f2fffd0fc8bd7cdb6244d99f021ff
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-28 21:50:46 -0400

    Add test for matrix operation

--- test/test_symmetryoperation.jl
+++ test/test_symmetryoperation.jl
@@ -65,6 +65,11 @@ using GroupTools
             u = MatrixOperation(cis(2π/3))
             @test isapprox(inv(u), MatrixOperation{ComplexF64}(cis(-2π/3)))
             @test isapprox(inv(u), MatrixOperation{1, ComplexF64}(cis(-2π/3)))
+
+            u = MatrixOperation([1 im; 0 im])
+            @test conj(u) == MatrixOperation([1 -im; 0 -im])
+            @test transpose(u) == MatrixOperation([1 0; im im])
+            @test adjoint(u) == MatrixOperation([1 0; -im -im])
         end
     end
 end
