2373e88e535016522d99cdd0762e530aeffc9461
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-29 08:01:00 -0400

    Update test for finitegroup

--- test/test_group.jl
+++ test/test_group.jl
@@ -93,6 +93,8 @@ using LinearAlgebra
         @test minimal_generating_set(group) == [2]
 
         @test group_multiplication_table([[1 0; 0 1], [1 0; 0 -1]]) == [1 2; 2 1]
+        @test_throws ArgumentError group_multiplication_table([[1 0; 0 1], [1 0; 0 1]]) # duplicate
+        @test_throws KeyError group_multiplication_table([[1 0; 0 1], [0 -1; 1 -1]]) # not closed
 
         @test ishomomorphic(group, 1:3; product=gp)
         @test !ishomomorphic(group, 1:2; product=gp)
