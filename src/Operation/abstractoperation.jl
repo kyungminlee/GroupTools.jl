95bbbb338ee82db077baac82cb9f41c0dd2aab72
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-30 02:34:01 -0400

    Add iterate and eltype to elements

--- src/Operation/abstractoperation.jl
+++ src/Operation/abstractoperation.jl
@@ -1,3 +1,7 @@
 export AbstractSymmetryOperation
 
 abstract type AbstractSymmetryOperation end
+
+Base.iterate(x::AbstractSymmetryOperation, ::Integer=1) = (x, nothing)
+Base.iterate(::AbstractSymmetryOperation, ::Nothing) = nothing
+Base.eltype(::Type{T}) where {T<:AbstractSymmetryOperation} = T
