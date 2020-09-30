c65b1c153c19b1e95f39a38a41753008b8937e59
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-30 02:07:16 -0400

    Add MatrixSymmetry

---
+++ src/Symmetry/matrixsymmetry.jl
@@ -0,0 +1,31 @@
+export MatrixSymmetry
+
+export elements
+export group
+
+struct MatrixSymmetry{M<:MatrixOperation}<:AbstractSymmetry
+    elements::Vector{M}
+    group::FiniteGroup
+    function MatrixSymmetry(matrices::AbstractVector{<:AbstractMatrix{S}}) where {S}
+        D = size(matrices[1], 1)
+        elements = MatrixOperation.(matrices)
+        group = FiniteGroup(generate_multiplication_table(elements))
+        return new{MatrixOperation{D, S}}(elements, group)
+    end
+end
+
+Base.eltype(::Type{MatrixSymmetry{M}}) where M = M
+Base.valtype(::Type{MatrixSymmetry{M}}) where M = M
+
+Base.length(x::MatrixSymmetry) = length(x.elements)
+Base.size(x::MatrixSymmetry) = (length(x.elements),)
+Base.keys(x::MatrixSymmetry) = Base.OneTo(length(x))
+
+Base.getindex(x::MatrixSymmetry, i) = Base.getindex(x.elements, i)
+
+Base.iterate(x::MatrixSymmetry) = Base.iterate(x.elements)
+Base.iterate(x::MatrixSymmetry, i) = Base.iterate(x.elements, i)
+
+
+elements(m::MatrixSymmetry) = m.elements
+group(m::MatrixSymmetry) = m.group
