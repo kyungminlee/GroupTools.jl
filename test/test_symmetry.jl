d71649876cf4a30d6bf2624ac10dc6ce26b5bf44
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-09-30 02:09:56 -0400

    Add tests for symmetry

---
+++ test/test_symmetry.jl
@@ -0,0 +1,37 @@
+using Test
+using GroupTools
+
+@testset "MatrixSymmetry" begin
+    sym1 = MatrixSymmetry([[1 0; 0 1], [-1 0; 0 -1], [0 -1; 1 0], [0 1; -1 0]])
+    sym2 = MatrixSymmetry([[1 0; 0 1], [0 -1; 1 -1], [-1 1; -1 0]])
+    sym3 = DirectProductSymmetry(sym1, sym2)
+
+    @test length(sym1) == 4
+    @test length(sym2) == 3
+    @test length(sym3) == 12
+
+    @test size(sym1) == (4,)
+    @test size(sym2) == (3,)
+    @test size(sym3) == (4, 3)
+
+    # sym3_collect1 = [sym3[i] for i in 1:length(sym3)]
+    # sym3_collect2 = [sym3[i] for i in eachindex(sym3)]
+    # sym3_collect3 = elements(sym3)
+    # @test sym3_collect1 == sym3_collect2
+
+    # @show keys(sym3)
+    @test size( [x for x in sym3] ) == (4,3)
+    @test Base.IteratorSize(sym3) == Base.HasShape{2}()
+    # @show [x for x in sym3] |> size
+    # for i in 1:length(sym3)
+    #     @show sym3[i]
+    # end
+
+    # @show eachindex(sym3)
+    # for x in sym3
+    #     @show x
+    # end
+    # @show elements(sym3)
+
+    # sym3[1,2]
+end
