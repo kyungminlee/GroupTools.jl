d8fe40578db05cc95e41c5a7d6343015a02fb19e
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-10-04 01:45:30 -0400

    Add iterator funcs for AbstractSymmetryOperation

--- src/Operation/abstractoperation.jl
+++ src/Operation/abstractoperation.jl
@@ -2,6 +2,9 @@ export AbstractSymmetryOperation
 
 abstract type AbstractSymmetryOperation end
 
+Base.IteratorSize(::Type{T}) where {T<:AbstractSymmetryOperation} = Base.HasShape{0}()
+Base.size(::AbstractSymmetryOperation) = ()
+Base.length(::AbstractSymmetryOperation) = 1
 Base.iterate(x::AbstractSymmetryOperation, ::Integer=1) = (x, nothing)
 Base.iterate(::AbstractSymmetryOperation, ::Nothing) = nothing
 Base.eltype(::Type{T}) where {T<:AbstractSymmetryOperation} = T
