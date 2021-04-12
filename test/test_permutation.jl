f92c839d749aba45e1e415c15ab865eebcbe3d00
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2021-04-11 23:50:10 -0400

    Add tests for permutation on vectors

--- test/test_permutation.jl
+++ test/test_permutation.jl
@@ -39,10 +39,21 @@ using GroupTools
     @test p0(1) == 1 && p0(2) == 2 && p0(3) == 3 && p0(4) == 4
     @test p1(1) == 2 && p1(2) == 3 && p1(3) == 4 && p1(4) == 1
 
+    @test_throws ArgumentError p1([1,2])
+    @test_throws ArgumentError p1([1,2,3,4,5])
+    
+    v = [10, 20, 30, 40]
+    @test p1(v) == [40, 10, 20, 30]  # act on column vector. (map from right to left)
+    p5 = Permutation([2,1,4,3])
+    @test p5(v) == [20, 10, 40, 30]
+    @test p1(p5(v)) == (p1*p5)(v)
+    @test p5(p1(v)) == (p5*p1)(v)
+
     @test_throws ArgumentError Permutation([1,2,3,4]) * Permutation([1,2,3,4,5])
     @test hash(Permutation(Int[1,2,3,4])) != hash(Int[1,2,3,4])
 
     @test generate_group(p1) == Set([p0, p1, p2, p3])
     @test generate_group(p2) == Set([p0, p2])
     @test generate_group(p1, p2) == Set([p0, p1, p2, p3])
+
 end
