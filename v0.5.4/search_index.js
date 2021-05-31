4d0c8c990f97ac8ac739f99af011095a555ca29f
Documenter.jl <documenter@juliadocs.github.io>
2021-05-31 12:12:50 +0000

    build based on c907f17

---
+++ v0.5.4/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GroupTools.jl","page":"Home","title":"GroupTools.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to GroupTools.jl.","category":"page"}]
+}
